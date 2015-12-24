0054825c7475c2e21d1225a48f824242e3c2c694
Colin <colingilbert86@gmail.com>
2015-12-24 01:03:37 -0600

    Initial commit

---
+++ dcollide-config.h
@@ -0,0 +1,14 @@
+// dcollide-config.h
+
+#ifndef DCOLLIDE_CONFIG_H
+#define DCOLLIDE_CONFIG_H
+
+// #define DCOLLIDE_USE_DOUBLE 1
+// #define DCOLLIDE_USE_THREADS 1
+// #define HAVE_ISNAN 1
+// #define HAVE_SYS_TIME_H 1
+// #define HAVE_EXECINFO_H 1
+
+// #define DCOLLIDE_BROADPHASE_DEBUG 1
+
+#endif
